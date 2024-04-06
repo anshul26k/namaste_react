fb4a7aca45823080a122560befbee9f073c771de
anshul <anshul26vk@gmail.com>
2024-04-06 08:41:46 +0530

    ep1

---
+++ App.js
@@ -0,0 +1,8 @@
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const temp =  React.createElement("h1",{id:"heading"},"hello bro");
+const temp =  React.createElement("div",{id:"parent"},React.createElement("div",{id:"headings"},[React.createElement("h1",{},"hello bro"),React.createElement("h1",{},"bye bro")]));
+
+console.log(temp)
+root.render(temp)
